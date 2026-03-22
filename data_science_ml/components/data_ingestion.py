03ce52ff6fba3dfaa25ab3e202db5d7daea54c23
Richard Kadey <richardkadey@gmail.com>
2026-03-22 18:59:41 +0000

    setup and logging added

---
+++ data_science_ml/components/data_ingestion.py
@@ -0,0 +1,6 @@
+from data_science_ml.logger import logging
+
+try:
+    logging.info("Testing")
+except Exception as e:
+    print(e)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "data_science_ml",
+    version = "0.0.0",
+    author = "Richard",
+    email = "richardkadey@gmail.com",
+    packages = find_packages()
+)
