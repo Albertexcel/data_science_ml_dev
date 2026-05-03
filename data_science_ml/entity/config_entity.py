83beee300b57aa117b4b616acf9e580793b70173
Richard Kadey <richardkadey@gmail.com>
2026-05-03 18:57:40 +0000

    data ingestion components added

--- data_science_ml/entity/config_entity.py
+++ data_science_ml/entity/config_entity.py
@@ -1,6 +1,5 @@
 import os
 from data_science_ml.constants import * 
-# from data_science_ml.constants import  ARTIFACT_DIR
 from dataclasses import dataclass
 from datetime import datetime
 
@@ -20,6 +19,10 @@ class DataIngestionConfig:
     training_file_path:str = os.path.join(data_ingestion_dir, "data_ingested", "train.csv")
     testing_file_path:str = os.path.join(data_ingestion_dir, "data_ingested", "test.csv")
 
-
-y = TrainingPipelineConfig().pipeline_name
-print(y)
+"""
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+    training_file_path:str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    testing_file_path:str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+"""
