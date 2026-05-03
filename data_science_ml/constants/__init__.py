c535b3e42ff1654f251a582b8747ef3ecbd02d1e
Richard Kadey <richardkadey@gmail.com>
2026-05-03 19:08:21 +0000

    data file added

---
+++ data_science_ml/constants/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+SERVER = "LAPTOP-Q9V35JK6\SQLEXPRESS;"
+DATABASE = "ML;"
+TABLE = "wine_quality_classification"
+ARTIFACT_DIR = "artifact"
+MODEL_FILE_NAME = "model.pkl"
+PIPELINE_NAME = "data_science_ml"
+
+TARGET_COLUMN = "quality_label"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH: str = os.path.join("config", "schema.yaml")
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+
+"""
+Data Ingestion related constants
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_INGESTED_DIR = "data_ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
